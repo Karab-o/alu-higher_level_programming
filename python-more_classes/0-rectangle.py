aa4d0a166333a972ba8d5d3d2606e927f5de76ec
Karab-o <k.divine@alustudent.com>
2024-11-09 09:44:13 -0800

    created and tested scripts

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 """
-This module defines a Rectangle class.
+New class Rectangle
 """
 
 
 class Rectangle:
-        """
-            Represents a Rectangle
-                """
-
-                    pass
+        """ Rectangle class empty """
+            pass
