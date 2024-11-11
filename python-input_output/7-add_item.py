b415f88b051eaa427785f29ce21cb0b327084b25
Karab-o <k.divine@alustudent.com>
2024-11-11 12:54:36 +0200

    created and tested scripts

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,7 +9,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 """ Script that adds all arguments to a Python list,
 and then save them to a file """
 try:
-        add_i = load_from_json_file('add_item.json')
-            save_to_json_file(add_i + argv[1:], 'add_item.json')
+    add_i = load_from_json_file('add_item.json')
+    save_to_json_file(add_i + argv[1:], 'add_item.json')
 except Exception:
-        save_to_json_file(argv[1:], 'add_item.json')
+    save_to_json_file(argv[1:], 'add_item.json')
