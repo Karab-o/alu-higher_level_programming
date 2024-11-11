1aaa27365a06ea9f71612eca97fe49ee45554292
Karab-o <k.divine@alustudent.com>
2024-11-11 12:47:33 +0200

    created and tested scripts

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -4,6 +4,6 @@ import json
 
 
 def from_json_string(my_str):
-        """ Returns an object (Python data structure)
-            represented by a JSON string """
-                return (json.loads(my_str))
+    """ Returns an object (Python data structure)
+    represented by a JSON string """
+    return (json.loads(my_str))
